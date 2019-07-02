fc914be17deb8685565e6d730b4f5511301093ef
yvonneyuting <45350322+yvonneyuting@users.noreply.github.com>
2019-07-02 17:55:38 +0800

    new commit

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <h1>Hello</h1>
       <input type="text" value={name} onChange={handleChange}/>
-      <div>輸入文字：{name}</div>
+      <div>輸入文字：{name}ccc</div>
     </div>
   );
 }
